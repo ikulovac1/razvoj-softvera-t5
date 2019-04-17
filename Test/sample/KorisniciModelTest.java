6fd47b5bd27f42eee9daf6d09532cb77fba01c1d
ikulovac1 <ikulovac1@etf.unsa.ba>
2019-04-17 10:11:08 +0200

    sesti tutorijal

--- Test/sample/KorisniciModelTest.java
+++ Test/sample/KorisniciModelTest.java
@@ -1,10 +1,28 @@
 package sample;
 
+import javafx.collections.ObservableList;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class KorisniciModelTest {
 
+private static KorisniciModel model = null;
 
 
+@BeforeAll
+    static void setup() {
+    model = new KorisniciModel();
+    model.napuni();
+}
+
+@Test
+void novogKorisnika() {
+    ObservableList<Korisnik> novi = model.getKorisnik();
+    novi.add(new Korisnik("semso", "semsic", "mail", "asdas", "sadas"));
+    assertEquals(5, model.getKorisnik().size());
+}
 
 }
+
